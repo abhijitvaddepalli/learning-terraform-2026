f4f8da7b94cd0c2b04ff48847f8db06215713ffb
abhijitvaddepalli <39533632+abhijitvaddepalli@users.noreply.github.com>
2026-07-31 13:59:40 +0100

    micro instance

--- main.tf
+++ main.tf
@@ -1,22 +1,6 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-0e0d2e3754385cbd3"
-  instance_type = var.instance_type
+  instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
